8e17fd060f39782a083bae3adb08a35eb5c59cfc
XxShenglixX <c_shengli@hotmail.com>
2015-05-21 12:44:16 +0800

    Added code for readBit(), readBits(), sendBit, sendBits Added SWDIO_OutputMode() and SWDIO_InputMode()

--- app/main.c
+++ app/main.c
@@ -4,6 +4,19 @@
 
 #define SWCLK_ON() HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, GPIO_PIN_SET);
 #define SWCLK_OFF() HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, GPIO_PIN_RESET);
+#define SWDIO_High() HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_SET);
+#define SWDIO_Low() HAL_GPIO_WritePin(GPIOB, GPIO_PIN_12, GPIO_PIN_RESET);
+
+void clockGenerator_1cycle();
+void configurePort();
+void SWDIO_OutputMode();
+void SWDIO_InputMode();
+void lineReset();
+void sendBit(int value);
+void sendBits(long dataToSend,int numberOfBits);
+int readBit();
+void readBits(long *dataRead,int numberOfBits);
+void clockGenerator_1cycle();
 
 void configurePort()
 {
@@ -11,7 +24,31 @@ void configurePort()
 	__GPIOB_CLK_ENABLE();
 
 	GpioInfo.Mode = GPIO_MODE_OUTPUT_PP ;
-	GpioInfo.Pin = GPIO_PIN_10 | GPIO_PIN_12; // Init PG13 and PG14
+	GpioInfo.Pin = GPIO_PIN_10 ; // PB10 as clock output
+	GpioInfo.Pull = GPIO_NOPULL ;
+	GpioInfo.Speed = GPIO_SPEED_HIGH ;
+
+	HAL_GPIO_Init(GPIOB,&GpioInfo);
+}
+
+void SWDIO_OutputMode()
+{
+	GPIO_InitTypeDef GpioInfo;
+
+	GpioInfo.Mode = GPIO_MODE_OUTPUT_PP ;
+	GpioInfo.Pin = GPIO_PIN_12; // PB12 as output
+	GpioInfo.Pull = GPIO_NOPULL ;
+	GpioInfo.Speed = GPIO_SPEED_HIGH ;
+
+	HAL_GPIO_Init(GPIOB,&GpioInfo);
+}
+
+void SWDIO_InputMode()
+{
+	GPIO_InitTypeDef GpioInfo;
+
+	GpioInfo.Mode = GPIO_MODE_INPUT ;
+	GpioInfo.Pin = GPIO_PIN_12; // PB12 as input
 	GpioInfo.Pull = GPIO_NOPULL ;
 	GpioInfo.Speed = GPIO_SPEED_HIGH ;
 
@@ -25,35 +62,44 @@ void lineReset()
 	sendBit(1);
 	for (i = 0 ; i < 50 ; i ++)
 	{
-		//clockGenerator_1cycle();
+		clockGenerator_1cycle();
 	}
+	sendBit(0);
 }
 
 void sendBit(int value)
 {
-	if ((value & 1) == 1)
-		;
+	if (value == 1)
+		SWDIO_High()
 	else
-		;
+		SWDIO_Low()
 }
 
 void sendBits(long dataToSend,int numberOfBits)
 {
-	int i ;
+	//LSB first
+
+	SWDIO_OutputMode();
+
+	int i , bitValue = 0;
 	for (i=0 ; i < numberOfBits ; i ++)
 	{
-		//sendBit();
+		bitValue = dataToSend & (1 << i ) ;
+
+		if (bitValue == 1)
+			sendBit(1);
+		else
+			sendBit(0);
 	}
 }
 
 int readBit()
 {
-	 //HAL_GPIO_ReadPin(GPIOA,GPIO_PIN_0);
-	//GPIO_PinState readBit ;
+	GPIO_PinState bitRead ;
 
-	//readBit =
+	bitRead  = HAL_GPIO_ReadPin(GPIOB,GPIO_PIN_12);
 
-	if (readBit == 1)
+	if (bitRead  == 1)
 		return 1 ;
 	else
 		return 0 ;
@@ -61,12 +107,14 @@ int readBit()
 
 void readBits(long *dataRead,int numberOfBits)
 {
-	int i = 0 ;
+	SWDIO_InputMode();
+
+	int i , bitRead;
 
 	for ( i = 0 ; i < numberOfBits ; i ++)
 	{
-		readBit();
-
+		bitRead = readBit();
+		*dataRead = *dataRead | (bitRead << i ) ;
 	}
 
 
