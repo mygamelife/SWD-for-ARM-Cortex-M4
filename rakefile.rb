e717d44b9d4e0829b79bc0b6fde9629e0de91e83
mygamelife <mygamelife1@gmail.com>
2015-11-06 15:02:52 +0800

    Edited custom project.yml command

--- rakefile.rb
+++ rakefile.rb
@@ -5,7 +5,7 @@ load "#{PROJECT_CEEDLING_ROOT}/lib/ceedling.rb"
 # issuing e.g.:  
 #     rake project=./myproject.yml test:all
 #
-project_file = (project_file = ENV['project']) ? String.new(project_file):'./project.yml'
+project_file = (project_file = ENV['p']) ? String.new(project_file):'./project.yml'
 raise ArgumentError, "Error: #{project_file} folder does not exist."  \
                                           if !File.exist?(project_file)
 Ceedling.load_project(config: project_file)
